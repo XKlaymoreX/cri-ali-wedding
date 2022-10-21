78bef92e23718026114656b518b2f62b003be5a1
Cristian Hoza <hoza.cristian01@gmail.com>
2022-10-21 08:18:05 +0200

    fix main section 1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css','../app.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  collapsed: boolean = false
 
   constructor() { }
 
