1ec90d39e7ce2e11933c555e3f249b5b58de7212
Cristian Hoza <hoza.cristian01@gmail.com>
2022-10-21 10:08:44 +0200

    main fix 3

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   collapsed: boolean = false
+  transparent: boolean = true
 
-  constructor() { }
+  @HostListener('window:resize', ['$event'])
+  onResize(event){
+    if(window.innerWidth < 768){
+      this.transparent = false
+    }else{
+      this.transparent = true
+    }
+  }
+
+  constructor() { 
+
+    if(window.innerWidth < 768){
+      this.transparent = false
+    }else{
+      this.transparent = true
+    }
+
+  }
 
   ngOnInit(): void {
   }
