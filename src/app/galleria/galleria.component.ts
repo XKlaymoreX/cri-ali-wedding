81bde1037eef015f8927144a8399e606e151cb3f
Cristian Hoza <hoza.cristian01@gmail.com>
2022-10-27 14:09:53 +0200

    local photos

--- src/app/galleria/galleria.component.ts
+++ src/app/galleria/galleria.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Database as FireDb, object } from '@angular/fire/database';
 import { Database } from '../db_interaction';
 
+
 @Component({
   selector: 'app-galleria',
   templateUrl: './galleria.component.html',
@@ -9,42 +10,43 @@ import { Database } from '../db_interaction';
 })
 export class GalleriaComponent implements OnInit {
 
+
+
   db: Database
   imageElements: HTMLImageElement[] = []
   loadedImages : number = 0
   showImages : boolean = false
   
   constructor(db:FireDb) {
+    
     this.db = new Database(db)
-    // this.db.createImagesNames()
 
-    this.db.retrieveData('immagini')
-    .then(res=> {
-      Object.keys(res).forEach((key, i) => {
-          let img = new Image(100,100)
+    let imagesNumber: number = 79
+    let counter:number = 0
 
-            img.onload = () => {
-              this.loaded()
-            }
-          img.src = Object.values(res)[i]
-          img.alt = key
-        this.imageElements.push(
-          img
-        )
-      })
+    do{
+      let img= new Image()
+      img.onload = () => {
+        this.loaded()
+      }
+      img.src = "assets/galleria/"+(counter+1).toString() + ".jpeg"
+      img.alt = 'img-'+(counter+1).toString()
+
+      console.log(img)
+      this.imageElements.push(img)
+      counter++
+    }while(counter < 79)
 
       this.imageElements = this.imageElements
         .map(value => ({value, sort:Math.random()}))
         .sort((a,b) => a.sort - b.sort)
         .map(({value}) => value)
-    })
-
-    
    }
 
 
    loaded = () => {
     this.loadedImages++
+    console.log(this.loadedImages)
     if(this.imageElements.length == this.loadedImages){
       this.showImages = true
     }
